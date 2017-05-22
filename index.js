109aca8446459bd6e87568034c6639a4f74cf1fa
kwisatzheadache <michaelhorvath87@gmail.com>
2017-05-22 17:06:39 -0400

    index.js runs retext

--- index.js
+++ index.js
@@ -6,13 +6,47 @@ var format = require('rehype-format');
 var html = require('rehype-stringify');
 var reporter = require('vfile-reporter');
 
-unified()
-  .use(markdown)
-  .use(remark2rehype)
-  .use(document)
-  .use(format)
-  .use(html)
-  .process('# Hello world!', function (err, file) {
+var retext = require('retext');
+var contractions = require('retext-contractions');
+var diacritics = require('retext-diacritics');
+var cliches = require('retext-cliches');
+var equality = require('retext-equality');
+var overuse = require('retext-overuse');
+var passive = require('retext-passive');
+var readability = require('retext-readability');
+var repeatedWords = require('retext-repeated-words');
+var sentiment = require('retext-sentiment');
+var smarypants = require('retext-smartypants');
+var spell = require('retext-spell');
+var usage = require('retext-usage');
+var profanities = require('retext-profanities');
+
+// unified()
+//   .use(markdown)
+//   .use(remark2rehype)
+//   .use(document)
+//   .use(format)
+//   .use(html)
+//   .process('# Hello world!', function (err, file) {
+//     console.error(reporter(err || file));
+//     console.log(String(file));
+//   });
+
+retext()
+  .use(contractions)
+  .use(diacritics)
+  .use(cliches)
+  .use(equality)
+  .use(overuse)
+  .use(passive)
+  .use(readability)
+  .use(repeatedWords)
+  .use(sentiment)
+  .use(smarypants)
+  .use(profanities)
+  //.use(spell)
+  .use(usage)
+  .process('hello world! how are you guys doing, shit fuck i dont konw?', function (err, file) {
     console.error(reporter(err || file));
     console.log(String(file));
   });
